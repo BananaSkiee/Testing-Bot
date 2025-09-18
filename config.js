6ccfc4cac204fce85dc09993f3bfbab533aa633b
MinimalMikir <bananaskiee@gmail.com>
2025-09-18 09:18:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,14 @@
 // config.js
+
 module.exports = {
-    TOKEN: process.env.TOKEN, // Pastikan ini mengambil dari env
-    voiceChannelId: "1360303391175217343",
-    logChannelId: "1393011303492092035"
+    // Ambil token dan ID server dari file .env
+    TOKEN: process.env.DISCORD_TOKEN,
+    GUILD_ID: process.env.GUILD_ID,
+
+    // Semua ID channel dikumpulkan di sini
+    CHANNELS: {
+        voiceChannelId: "1360303391175217343",
+        logChannelId: "1393011303492092035",
+        autoGreetingChannelId: "1352404526870560788"
+    }
 };
